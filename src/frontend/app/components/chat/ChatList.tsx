7438c9c04026f74cf010715062e336d404dc7d26
Nils <nilshassler@gmail.com>
2026-01-15 22:33:15 +0100

    add ChatItem

--- src/frontend/app/components/chat/ChatList.tsx
+++ src/frontend/app/components/chat/ChatList.tsx
@@ -1,4 +1,31 @@
 import { type ChatResponse } from "../../types/api"
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemTitle,
+  ItemGroup
+} from "../ui/item"
+
+interface ChatItemProps {
+  chat: ChatResponse
+  onClick: () => void
+}
+
+const ChatItem = ( {chat, onClick}: ChatItemProps) => {
+  return (
+  <div className="flex w-full max-w-md flex-col gap-6" onClick={onClick}>
+        <Item variant="outline">
+          <ItemContent>
+            <ItemTitle>{`There is no title yet, so here is an id: ${chat.id}`}</ItemTitle>
+            <ItemDescription>
+              A dummy description
+            </ItemDescription>
+          </ItemContent>
+        </Item>
+    </div>
+  )  
+}
 
 export interface ChatListProps {
   chats: ChatResponse[],
@@ -7,15 +34,11 @@ export interface ChatListProps {
 
 export const ChatList = ( { chats, openChat }: ChatListProps ) => {
     return (
-      chats.map((chat, index) => 
-        {
-          return (
-            <div key={index} onClick={() => 
-            {
-              openChat(index)
-            }}>{chat.createdAt}</div>
-          )
-        }
-    ))
+      <ItemGroup>
+        {chats.map((chat, index) => (
+            <ChatItem key={`chat-${index}`} chat={chat} onClick={() => openChat(index)}/>
+          ))}
+      </ItemGroup>      
+    )
 }
 
