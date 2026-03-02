1ae498228708da757164a72c9aee654c49a84c69
Nils <nilshassler@gmail.com>
2026-03-02 08:27:16 +0100

    fix message theme in light mode

--- src/frontend/app/components/chat/ChatMessage.tsx
+++ src/frontend/app/components/chat/ChatMessage.tsx
@@ -4,11 +4,13 @@ export interface ChatMessageProps {
 }
 
 export const ChatMessage = ({ content, role }: ChatMessageProps ) => {
-    const className = role === "USER" ? "self-end bg-neutral-700" : "self-start bg-blue-600" 
+    const className = role === "USER" 
+        ? "self-end" 
+        : "self-start"
 
     return(
-        <div className={`m-1 inline-block max-w-[70%] bg-slate-700 rounded-xl ${className}`}>
-            <h1 className="p-2">{content}</h1>
+        <div className={`m-1 inline-block max-w-[70%] rounded-xl p-2 bg-secondary text-secondary-foreground ${className}`}>
+            <h1>{content}</h1>
         </div>
     )
 }
