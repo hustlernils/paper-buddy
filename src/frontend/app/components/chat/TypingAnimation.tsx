51192b6a0177ab9e229ae95c8da2584528aca266
Nils <nilshassler@gmail.com>
2026-03-02 11:34:32 +0100

    use delay class for animation

--- src/frontend/app/components/chat/TypingAnimation.tsx
+++ src/frontend/app/components/chat/TypingAnimation.tsx
@@ -3,16 +3,12 @@ export const TypingAnimation = () =>
   return (
     <div className="flex items-center space-x-1">
       <span 
-        className="w-2 h-2 bg-secondary-foreground rounded-full animate-bounce inline-block"
-        style={{ animationDelay: '0s' }}
+        className="w-2 h-2 bg-secondary-foreground rounded-full animate-bounce inline-block"/>
+      <span 
+        className="w-2 h-2 bg-secondary-foreground rounded-full animate-bounce inline-block delay-150"
       />
       <span 
-        className="w-2 h-2 bg-secondary-foreground rounded-full animate-bounce inline-block"
-        style={{ animationDelay: '0.2s' }}
-      />
-      <span 
-        className="w-2 h-2 bg-secondary-foreground rounded-full animate-bounce inline-block"
-        style={{ animationDelay: '0.4s' }}
+        className="w-2 h-2 bg-secondary-foreground rounded-full animate-bounce inline-block delay-300"
       />
     </div>
   )
